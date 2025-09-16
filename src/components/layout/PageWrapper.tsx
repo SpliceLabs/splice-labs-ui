d111e84afbc64f5b92954090b3fc8e7011e2f05a
dreadful-dev <jake@wlabel.co>
2025-09-16 16:46:38 -0400

    add mobile fixes

--- src/components/layout/PageWrapper.tsx
+++ src/components/layout/PageWrapper.tsx
@@ -7,7 +7,7 @@ interface PageWrapperProps {
 
 export default function PageWrapper({ children, className = '' }: PageWrapperProps) {
   return (
-    <div className={`min-h-screen flex flex-col justify-center pt-20 ${className}`}>
+    <div className={`min-h-screen flex flex-col justify-center pt-24 sm:pt-20 ${className}`}>
       {children}
     </div>
   );
